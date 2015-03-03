aa8ecd252cf434ea60019e404e1fe488b07bb05e
Andrew Smith <andrew@WeAreSoftwareCraftsmen.com>
2015-03-02 21:54:49 -0500

    #17 Html component specs

--- src/server/Html.jsx
+++ src/server/Html.jsx
@@ -7,8 +7,9 @@ export default React.createClass({
 		// Only for production. For dev, it's handled by webpack with livereload.
 		let linkStyles = this.props.isProduction &&
 			<link
-				href={`/this.props.assets.public/app.css?v=${this.props.version}`}
+				href={`/${this.props.assets.public}/app.css?v=${this.props.version}`}
 				rel="stylesheet"
+				ref="appStyles"
 			/>;
 
 		return (
@@ -16,14 +17,14 @@ export default React.createClass({
 				<head>
 					<meta charSet="utf-8" />
 					<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-					<title>{this.props.title}</title>
+					<title ref="title">{this.props.title}</title>
 					{linkStyles}
 					<meta name="viewport" content="width=device-width, user-scalable=no" />
 					<meta name="HandheldFriendly" content="True" />
-					<link rel="canonical" href={ this.props.blog.url } />
-					<link rel="alternate" type="application/rss+xml" title={ this.props.blog.title } href={ this.props.blog.url + '/rss/' } />
+					<link rel="canonical" href={ this.props.blog.url } ref="canonical" />
+					<link rel="alternate" type="application/rss+xml" title={ this.props.blog.title } href={ this.props.blog.url + '/rss' } ref="rss" />
 				</head>
-				<body dangerouslySetInnerHTML={{__html: this.props.bodyHtml}} />
+				<body dangerouslySetInnerHTML={{__html: this.props.bodyHtml}} ref="bodyEl" />
 			</html>
 		);
 	}
--- tests/lib/specs-setup.js
+++ tests/lib/specs-setup.js
@@ -7,3 +7,7 @@ chai.should();
 chai.use(sinonChai);
 chai.use(chaiAsPromised);
 var sinonAsPromised = require('sinon-as-promised');
+var jsdom = require('jsdom');
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.window = document.parentWindow;
+global.navigator = global.window.navigator;
