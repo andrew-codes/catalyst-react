7db30aa5c0fbd88d1b27dd125702842f70a2ce52
Andrew Smith <andrew@WeAreSoftwareCraftsmen.com>
2015-03-08 13:07:41 -0400

    #10 Stylus middleware should send transpiled CSS content

--- src/server/middlewares/stylus.js
+++ src/server/middlewares/stylus.js
@@ -1,13 +1,15 @@
 'use strict';
 
-import cacheStore from './../staticCache/cacheStore';
+import stylusTranspiler from './transpilers/stylusTranspiler';
 
 export default Object.freeze({
-	route: '/**/*.styl',
-	execute: (request, response) =>{
-		let css = cacheStore(request.url);
-		response.set('content-type', 'text/css')
-			.status(200)
-			.send(css);
-	}
+    route: '/**/*.styl',
+    execute: (request, response) => {
+        stylusTranspiler(request.url)
+            .then(css => {
+                response.set('content-type', 'text/css')
+                    .status(200)
+                    .send(css);
+            });
+    }
 });
--- test/specs/server/middlewares/stylus.Specs.js
+++ test/specs/server/middlewares/stylus.Specs.js
@@ -4,58 +4,55 @@ import proxy from 'proxyquire';
 import sinon from 'sinon';
 
 describe('stylus middleware', function () {
-	beforeEach(() => {
-		this.cacheStore = sinon.stub();
-	});
+    beforeEach(() => {
+        this.sut = null;
+    });
+    describe('when requiring the middleware', () => {
+        beforeEach(() => {
+            this.sut = proxy('./../../../../src/server/middlewares/stylus', {});
+        });
+        it('it should return a frozen object', () => {
+            (() => {
+                this.sut.route = 'me';
+            }).should.throw();
+        });
+    });
 
-	describe('when requiring the middleware', () => {
-		beforeEach(() => {
-			this.sut = proxy('./../../../../src/server/middlewares/stylus', {});
-		});
-		it('it should return a frozen object', () => {
-			(() => {
-				this.sut.route = 'me';
-			}).should.throw();
-		});
-	});
-
-	describe('when getting its route', () => {
-		beforeEach(() => {
-			this.sut = proxy('./../../../../src/server/middlewares/stylus', {});
-			this.actual = this.sut.route;
-		});
-		it('it should be a route matching any file with a stylus (.styl) extension', () => {
-			this.actual.should.be.equal('/**/*.styl');
-		});
-	});
-	describe('given a server\'s request and response for a stylus file', () => {
-		beforeEach(() => {
-			this.request = {
-				url: 'path/to/stylus/file'
-			};
-			this.response = {};
-			this.response.set = sinon.stub().returns(this.response);
-			this.response.status = sinon.stub().returns(this.response);
-			this.response.send = sinon.stub().returns(this.response);
-		});
-		describe('given the stylus has not already been translated to css', () => {
-			beforeEach(() => {
-				this.expectedCss = 'some css';
-				this.cacheStore = sinon.stub().withArgs('/path/to/stylus/file').returns(this.expectedCss);
-			});
-			describe('when executing the middleware', () => {
-				beforeEach(() => {
-					this.sut = proxy('./../../../../src/server/middlewares/stylus', {
-						'./../staticCache/cacheStore': this.cacheStore
-					});
-					this.sut.execute(this.request, this.response);
-				});
-				it('it should transpile the stylus to css to send as the response', () => {
-					this.response.set.should.have.been.calledWith('content-type', 'text/css');
-					this.response.status.should.have.been.calledWith(200);
-					this.response.send.should.have.been.calledWith(this.expectedCss);
-				});
-			});
-		});
-	});
+    describe('when getting its route', () => {
+        beforeEach(() => {
+            this.sut = proxy('./../../../../src/server/middlewares/stylus', {});
+            this.actual = this.sut.route;
+        });
+        it('it should be a route matching any file with a stylus (.styl) extension', () => {
+            this.actual.should.be.equal('/**/*.styl');
+        });
+    });
+    describe('given a server\'s request and response for a stylus file', () => {
+        beforeEach(() => {
+            this.request = {
+                url: 'path/to/stylus/file'
+            };
+            this.response = {};
+            this.response.set = sinon.stub().returns(this.response);
+            this.response.status = sinon.stub().returns(this.response);
+            this.response.send = sinon.stub().returns(this.response);
+        });
+        describe('when executing the route', () => {
+            beforeEach(() => {
+                this.expectedCss = 'some css';
+                this.stylusTranspiler = sinon.stub().withArgs(this.request.url).resolves(this.expectedCss);
+                this.sut = proxy('./../../../../src/server/middlewares/stylus', {
+                    './transpilers/stylusTranspiler': this.stylusTranspiler
+                });
+                this.sut.execute(this.request, this.response);
+            });
+            it('should set the content type to be CSS', () => {
+                this.stylusTranspiler().then(()=> {
+                    this.response.set.should.have.been.calledWith('content-type', 'text/css');
+                    this.response.status.should.have.been.calledWith(200);
+                    this.response.send.should.have.been.calledWith(this.expectedCss);
+                });
+            });
+        });
+    });
 });
