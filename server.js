854dc8e8cd051fd7bdee6eb2ce31094d129eb745
Andrew Smith <andrew@WeAreSoftwareCraftsmen.com>
2015-03-16 13:52:55 -0400

    Move blog config to blog directory for easy access when creating a new blog

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 require('babel/register');
-var config = require('./blog.config');
+var config = require('./blog/blog.config');
 
 var server = require('./src/server/Server');
 server(config).start();
